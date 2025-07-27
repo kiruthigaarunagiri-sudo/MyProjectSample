a8705eeee0f3a12e11deda494b06d92a7a608435
kiruthiga <Kiruthigaarunagiri@gmail.com>
2025-07-27 19:10:57 +0530

    First time configure

---
+++ src/test/java/Sample.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+		System.out.println("its done");
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the age");
+		int age=s.nextInt();
+		System.out.println("Enter the person age:"+age);
+		if (age==18) {
+			System.out.println("Person is elegible to vote");
+		} else {
+			System.out.println("Persobn is not elegible to vote");
+
+		}
+		System.out.println("its done");
+		System.out.println("its done");
+			
+	}
+}
